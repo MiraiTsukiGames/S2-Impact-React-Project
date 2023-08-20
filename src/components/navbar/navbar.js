9b60292db70d44f9939ce3cd9e33523b279db99b
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-20 16:10:29 +0200

    update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+
   return (
-    <nav>
-      <Link to="/" className="title">
+    <nav className="navigation">
+      <Link to="/" className="brand-name">
         Website
       </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className={menuOpen? "open" : ""}>
-        <li>
+      <button className="hamburger" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+          </svg>
+      </button>
+      <div className={isNavExpanded ? "menu-expanded" : "navigation-menu"}>
+        <ul>
+          <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
+              <li>
           <Link to="/timer">Timer</Link>
         </li>
       </ul>
+      </div>
     </nav>
   );
 };
