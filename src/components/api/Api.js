830bf152a6206035ef09b7a23509b27712e144b9
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-20 17:10:31 +0200

    update

--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Button from "../button/Button";
+import styles from './Api.module.css';
 
 const Quotes = () => {
   const [quote, setQuote] = useState("");
@@ -17,9 +18,10 @@ const Quotes = () => {
     };
 
     return (
-      <div className="content">
-        <p>{quote}</p>
-        <p>{author}</p>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Quotes</h2>
+        <p className={styles.quotes}>{quote}</p>
+        <p  className={styles.author}>{author}</p>
         <Button onClick={getQuote} text="Quotes"/>
       </div>
     );
--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -3,23 +3,23 @@ import Quote from '../components/api/Api';
 import Countdown  from '../components/timercomponent/Countdown';
 import Button from '../components/button/Button';
 import Track from '../components/audiocomponent/Audio';
-
+import styles from './Timer.module.css';
 
 const Timer = () => {
   const audioRef = useRef(new Audio());
   
 return (
-    <div className="container">
+    <div className={styles.container}>
       <>
       <Countdown  audioRef={audioRef}/>
       </>
       <>
       <Track audioRef={audioRef} />
       </>
-          <div className="quotes">
-            <Quote />
-        </div>
-        <Button onClick={() => window.location.href = '/'} text="Home"/>
+      <section className="quotes">
+      <Quote />
+      </section>
+      <Button onClick={() => window.location.href = '/'} text="Home"/>
        </div>
        );
 }
