6d44e77d8f0f5e14ac4cd54ec376c323cbe7f77f
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-22 19:42:38 +0200

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,16 @@ import React from "react";
 import Button from "../components/button/Button";
 import Title from "../components/Title/Title";
 import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <section className={styles.banner}>
       <Title testo="Welcome to Borcelle Yoga Studio" />
       <p>Your healthcare is our mission.</p>
       <Button
-        onClick={() =>{window.location.href= "/timer"}}
+        onClick={() =>navigate("/timer")}
         text="Getting started"
       />
     </section>
