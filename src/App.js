0e984c96c755dcf41c8fc13401f618b3d7d0ba12
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-23 23:21:48 +0200

    up

--- src/App.js
+++ src/App.js
@@ -22,21 +22,23 @@ function App() {
       <header className="App-header">
         <Navbar />
         <span className="Mode">{contextTheme} Mode</span>
+        <label htmlFor="material-switch">
         <ReactSwitch
-          onChange={handleSwitch}
-          checked={checked}
-          onColor="#86d3ff"
-          onHandleColor="#2693e6"
-          handleDiameter={15}
-          uncheckedIcon={false}
-          checkedIcon={false}
-          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-          height={15}
-          width={30}
-          className="react-switch"
-          id="material-switch"
+            checked={checked}
+            onChange={handleSwitch}
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={15}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            height={15}
+            width={30}
+            className="react-switch"
+            id="material-switch"
         />
+         </label>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
