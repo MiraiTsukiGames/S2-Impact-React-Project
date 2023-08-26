b249dc18affe11315f77c150192d1b9bdf572407
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-26 15:51:00 +0200

    up

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-export default function button(props) {
+export default function Button(props) {
   return (
     <button
       className={styles.button}
       onClick={props.onClick}
-    >
+    > {/*Button props onClick and text */}
       {props.text}
-    </button>
-  );
+    </button> 
+  ); 
 }
