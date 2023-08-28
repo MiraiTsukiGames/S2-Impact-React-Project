d63c766eba5a183cfaf186735fcca7e6cc7e22ac
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-28 20:51:15 +0200

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,11 +10,8 @@ const Home = () => {
   return (
     <section className={styles.banner}>
       <div className={styles.card}>
-        {/* Component for the section title */}
         <Title text="Welcome to Borcelle Yoga Studio" />
-          {/* Text paragraph */}
         <p>Your healthcare is our mission.</p> 
-        {/* Button to initiate navigation to the "timer" page */}
         <Button onClick={() => navigate("timer")} text="Getting started" />
       </div>
     </section>
