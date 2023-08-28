83f2bb9da058fcfb5922f13bc787f102f3e8ba2b
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-28 20:28:08 +0200

    up

--- src/components/button/Playbutton.js
+++ src/components/button/Playbutton.js
@@ -4,6 +4,7 @@ import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Playbutton.module.css";
 
 const SpecificButton = ({ onClick, isActive }) => {
+  // Render a button with a play or pause icon based on the isActive prop
   return (
     <button title="play" className={styles.button} onClick={onClick}>
       <FontAwesomeIcon icon={isActive ? faPause : faPlay} />
