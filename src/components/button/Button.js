2bf376552137a8a5acd86ba0a4d53b3063385f89
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-28 20:36:55 +0200

    up

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,7 +4,6 @@ import styles from "./Button.module.css";
 export default function Button(props) {
   return (
     <button className={styles.button} onClick={props.onClick}>
-      {/*Button props onClick and text */}
       {props.text}
     </button>
   );
