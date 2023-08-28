fa5b2697e859c9cfc10d1dde9355a19834b523b8
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-28 20:49:00 +0200

    up

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { ThemeContextProvider } from "./context/Themecontext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <React.StrictMode>
   <ThemeContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ThemeContextProvider>,
+  </ThemeContextProvider>
+  </React.StrictMode>,
 );
