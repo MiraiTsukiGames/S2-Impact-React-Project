ffb87757e67847d053ad630a209ad95e1ff31edf
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-31 12:03:06 +0200

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,22 @@ import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
-const Home = () => {
+export default function Home() {
   const navigate = useNavigate(); // Get the navigation function from react-router-dom
 
   return (
-    <Helmet>
     <section className={styles.banner}>
+    <Helmet>
+    <meta charSet="utf-8" />
+    <title>Borcelle Yoga Studio</title>
+    <meta name="description" content="meditation app" />
+    </Helmet>
       <div className={styles.card}>
         <Title text="Welcome to Borcelle Yoga Studio" />
         <p>Your healthcare is our mission.</p>
         <Button onClick={() => navigate("timer")} text="Getting started" />
       </div>
     </section>
-    </Helmet>
   );
 };
 
-export default Home;
